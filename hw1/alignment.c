97a140ae48ea7be766cf1558e8911b86c6631aaf
shaochun1023 <shaochun@vt.edu>
2023-08-12 22:37:30 -0400

    gcc

--- hw1/alignment.c
+++ hw1/alignment.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
 
 static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
@@ -15,11 +16,21 @@ int main() {
     uintptr_t aligned_121 = align_up(121, 4);
     uintptr_t aligned_122 = align_up(122, 4);
     uintptr_t aligned_123 = align_up(123, 4);
-    
+
+    uintptr_t aligned_128 = align_up(128, 16);
+    uintptr_t aligned_129 = align_up(129, 16);
+    uintptr_t aligned_130 = align_up(130, 16);
+    uintptr_t aligned_131 = align_up(131, 16);
+
     printf("align_up(120, 4) = %lu\n", aligned_120);
     printf("align_up(121, 4) = %lu\n", aligned_121);
     printf("align_up(122, 4) = %lu\n", aligned_122);
     printf("align_up(123, 4) = %lu\n", aligned_123);
+
+    printf("align_up(128, 16) = %lu\n", aligned_128);
+    printf("align_up(129, 16) = %lu\n", aligned_129);
+    printf("align_up(130, 16) = %lu\n", aligned_130);
+    printf("align_up(131, 16) = %lu\n", aligned_131);
     
     return 0;
 }
